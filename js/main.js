8de5ed2b2fd3de6627e23f1a67a261bc5075c8ac
yakuznecov <yakuznecov@mail.ru>
2021-03-08 21:30:37 +0300

    add js gallery

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var btn = document.getElementsByClassName("top__gallery-item");
+var gallery = document.getElementById("gallery");
+
+var images = new Array(
+    "img/pic1.jpg",
+    "img/pic2.jpg",
+    "img/pic3.jpg",
+    "img/pic4.jpg"
+);
+
+for(let i=0; i<btn.length; i++){
+    btn[i].onclick = function(){
+        gallery.src = images[i];
+        let current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace("active", "");
+        this.className += " active";
+    };
+}
