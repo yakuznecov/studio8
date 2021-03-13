01cf35e5254d45c64e46387466b8fc697f9523b0
yakuznecov <yakuznecov@mail.ru>
2021-03-13 06:29:21 +0300

    add menu js

--- js/main.js
+++ js/main.js
@@ -16,3 +16,23 @@ for(let i=0; i<btn.length; i++){
         this.className += " active";
     };
 }
+
+
+let iconMenu = document.querySelector(".icon-menu");
+if (iconMenu != null) {
+	let delay = 500;
+	let menuBody = document.querySelector(".menu__body");
+	iconMenu.addEventListener("click", function (e) {
+		if (unlock) {
+			body_lock(delay);
+			iconMenu.classList.toggle("_active");
+			menuBody.classList.toggle("_active");
+		}
+	});
+};
+function menu_close() {
+	let iconMenu = document.querySelector(".icon-menu");
+	let menuBody = document.querySelector(".menu__body");
+	iconMenu.classList.remove("_active");
+	menuBody.classList.remove("_active");
+}
